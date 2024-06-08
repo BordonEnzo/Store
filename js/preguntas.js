30956a9c9dfd86a2770a4e4abcd939a0569c115d
Enzo Bordon <enzobordon15@gmail.com>
2024-06-07 21:56:00 -0300

    primer commit

---
+++ js/preguntas.js
@@ -0,0 +1,10 @@
+// var  nombreUsuario = prompt("¿Cuál es su nombre?")
+// alert('bienvenid@ '+nombreUsuario)
+// alert('a continuación le haremos una serie de preguntas a fin de recolectar información a cerca del interes de nuestros clientes')
+
+// var primeravez = prompt("¿es su primera  vez en el sitio web? | si | no | ")
+// var hardware = prompt("¿Tiene conocimientos previos acerca de Hardware? | si | no |")
+// var gamas = prompt("¿Que gama de computadora busca? | alta | media | baja |")
+// var problemas = prompt("¿Suele tener problemas de almacenamiento? | si | no |")
+// var  correo = prompt ("¿Le gustaria recibir promociones semanales por correo electronico? | si | no |")
+
---
+++ main.js
@@ -0,0 +1,88 @@
+$('.productos-1').owlCarousel({
+    autoplay:true,
+    autoplaytimeout:1,
+    autoplayhoverpause:true,
+    dots:false,
+    loop:true,
+    margin:80,
+    nav:false,
+    lazyload: true,
+    responsive:{
+        0:{
+            items:2,
+            margin:4
+        },
+        600:{
+            items:3
+            
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+$('.productos-2').owlCarousel({
+    autoplay:false,
+    loop:true,
+    margin:80,
+    nav:false,
+    dots:false,
+    
+    responsive:{
+        0:{
+            items:2,
+            margin:4
+        },
+        600:{
+            items:3
+            
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+$('.productos-3').owlCarousel({
+    autoplay:false,
+    loop:true,
+    margin:80,
+    nav:false,
+    dots:false,
+    responsive:{
+        0:{
+            items:2,
+            margin:4
+        },
+        600:{
+            items:3
+            
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+$('.owl-carousel').owlCarousel({
+    autoplay:false,
+    loop:true,
+    margin:80,
+    nav:false,
+    dots:false,
+    
+    responsive:{
+        0:{
+            items:2,
+            margin:4
+        },
+        600:{
+            items:3
+            
+        },
+        1000:{
+            items:5
+        }
+    }
+})
